5018d72a1386f8cfe06f42d06d1a241b96f9e579
Bobby Brice <bobby.brice@gmail.com>
2019-12-23 22:28:36 -0500

    added axios get request and search fn

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
 import React, { Component } from 'react';
-import { Provider } from './components/context';
-import logo from './logo.svg';
-import './App.css';
-
+import axios from 'axios';
+import apiKey from './config';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
 import Nav from './components/Nav';
 import SearchForm from './components/SearchForm';
 import Gallery from './components/Gallery';
 import NotFound from './components/NotFound';
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      photos: [],
+      loading: true
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    this.performSearch();
+  }
+
+  performSearch = (query) => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&content_type=1&per_page=24&format=json&nojsoncallback=1`)
+      .then(resData => {
+        this.setState({
+          photos: resData.data.photos.photo,
+          loading: false
+        });
+      })
+      .catch(error => {
+        console.log("Error fetching and parsing data", error);
+      });
+  }
+
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    )
+  }
+
 }
+
 
 export default App;
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,4 +4,20 @@ import GalleryItem from './GalleryItem';
 import NotFound from './NotFound';
 
 
-// export default Gallery;
+class Gallery extends PureComponent {
+  render () {
+    return (
+      <div class="photo-container">
+        <h2>Results</h2>
+        <ul>
+          <li>
+            <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
+          </li>
+        </ul>
+      </div>          
+    );
+  }
+}
+
+
+export default Gallery;
